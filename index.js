348f7485e58fa35dd329bebd111978762987fa11
ahmed <ahmed.high212@gmail.com>
2023-05-17 20:18:25 +0530

    created bill splitter website

---
+++ index.js
@@ -0,0 +1,77 @@
+const billInput = document.querySelector(".bill-input");
+const tipPerPerson = document.getElementById("tip-amount");
+const totalPerPerson = document.getElementById("total-amount");
+const tips = document.querySelectorAll(".box");
+const genbill = document.querySelector(".gen");
+// const disabled=document.querySelector(".disabled")
+
+billInput.addEventListener("input", billInputfun);
+genbill.addEventListener("click", genbillfun);
+
+tipPerPerson.innerHTML = "₹" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "₹" + (0.0).toFixed(2);
+billInput.value = "0";
+
+
+function billInputfun() {
+  billValue = parseFloat(billInput.value);
+  genbillfun(); 
+}
+
+
+
+
+let billValue = 0.0;
+let tipValue = 0.15;
+
+tips.forEach(function (val) {
+    val.addEventListener("click", handleclick);
+});
+
+function handleclick(event) {
+    tips.forEach(function (val) {
+    if (event.target.innerHTML == val.innerHTML) {
+        tipValue = parseFloat(val.innerHTML) / 100;
+    }
+});
+// genbillfun();    
+}
+function genbillfun() {
+
+if (billInput.value !== "0" && count > 0 ) {
+    let tipAmount = (billValue + tipValue) / count;
+    let total = (billValue * tipAmount) / count;
+    tipPerPerson.innerHTML = "₹" + tipAmount.toFixed(2);
+    totalPerPerson.innerHTML = "₹" + total.toFixed(2);
+}
+
+}
+
+let counter = document.getElementById("counter");
+let increament = document.getElementsByClassName("incr")[0];
+let decreament = document.getElementsByClassName("decr")[0];
+let reset = document.getElementsByClassName("reset")[0];
+
+let count = 0;
+increament.addEventListener("click", function () {
+  count++;
+  counter.innerHTML = count;
+});
+decreament.addEventListener("click", function () {
+  count--;
+  counter.innerHTML = count;
+});
+reset.addEventListener("click", function () {
+  if (count < 0 || count > 0) {
+    count = 0;
+  }
+//   billInput.value='0.0'
+// billInputfun()
+counter.innerHTML = count;
+billInput.value = count;
+  totalPerPerson.innerHTML='₹0.00';
+  tipPerPerson.innerHTML='₹0.00';
+
+  
+});
+
